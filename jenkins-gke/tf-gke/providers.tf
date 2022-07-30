3c4193072c50e12998a9391c2085fdb99dfc7ada
Philip Hines <philip@PhilipMjorieMBP.home.arpa>
2022-07-30 18:52:05 +0100

    updates

--- jenkins-gke/tf-gke/providers.tf
+++ jenkins-gke/tf-gke/providers.tf
@@ -41,7 +41,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.3.0"
+      version = ">= 4.30.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
